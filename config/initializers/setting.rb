f910f43bf96b5ad3dc930d91b6345308ae02e98e
William McGuire <bmcguire@myqueball.com>
2011-11-23 12:06:55 -0700

    weight model pre-cleanup

---
+++ config/initializers/setting.rb
@@ -0,0 +1,12 @@
+$week0 = Time.local(2010,1,8).strftime("%m-%d")
+$week2 = Time.local(2010,1,22).strftime("%m-%d")
+$week4 = Time.local(2010,2,5).strftime("%m-%d")
+$week6 = Time.local(2010,2,19).strftime("%m-%d")
+$week8 = Time.local(2010,3,5).strftime("%m-%d")
+$week10 = Time.local(2010,3,19).strftime("%m-%d")
+$week12 = Time.local(2010,4,2).strftime("%m-%d")
+$a=[$week0,$week2,$week4,$week6,$week8,$week10,$week12]
+
+$buyin = '50'
+
+$weeks = ['1','2','3','4','5','6','7','8']
